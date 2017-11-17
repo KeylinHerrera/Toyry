5664e7590046bf8462ba9778ffe589fbebe115e2
Keylin Herrera Esquivel <kherrera@b7-11.local>
2017-11-17 15:01:32 -0600

    Changes

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+var toys = ['img/artefacto_small.png, img/artefacto_small.png, img/astronauta_small.png, img/astronauta_small.png, img/bus_small.png, img/bus_small.png, img/muñeca_small.png, img/muñeca_small.png, img/oso_small.png, img/oso_small.png, img/peluche_small.png, img/peluche_small.png, img/perro_small.png, img/perro_small.png, img/robot_small.png, img/robot_small.png, img/soldado_small.png, img/soldado_small.png, img/triciclo_small.png,img/triciclo_small.png']
+var imgs = document.getElementsByClassName('toys')
+
+function shuffle (array) {
+  var currentIndex = array.length
+  var tempValue
+  var randomIndex
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    tempValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = tempValue
+  }
+
+  return array
+}
+
+var toysRandom = shuffle(toys)
+
+Array.from(imgs).forEach(function (element, index) {
+  element.src = toysRandom[index]
+})
+
+console.log(toysRandom)
